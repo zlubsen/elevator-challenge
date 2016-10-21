381bda18f0d85efaca328678f5d47263c5d1e274
Zeeger Lubsen <zeeger@lubsen.eu>
2016-10-21 22:17:52 +0200

    Solves #1
    Basic moving works ok, but waitingQueue is not emptied correctly.
    Added plenty of logging for debugging.

--- elevator-challenge.js
+++ elevator-challenge.js
@@ -4,6 +4,7 @@
 		var waitingQueue = []; // queue of floornumbers where people are waiting for an elevator.
 
     for(var elevatorNo in elevators) {
+        elevators[elevatorNo].id = elevatorNo;
         registerElevatorEvents(elevators[elevatorNo]);
     }
     for(var floorNo in floors) {
@@ -12,27 +13,29 @@
 
     function registerElevatorEvents(elevator) {
       elevator.on("idle", function() {
-        console.log("elevator idle at: " + elevator.currentFloor());
+        console.log("Elevator " + elevator.id + " at " + elevator.currentFloor() + ": idle at floor " + elevator.currentFloor());
 
-        setElevatorAvailable(elevator);
+        //setElevatorAvailable(elevator);
 
         // Go to the first floor where someone is waiting, remove request from waitingQueue.
         if(waitingQueue.length > 0) {
-          console.log("going to floor " + waitingQueue[0] + ", because someone is waiting there.");
+          console.log("Elevator " + elevator.id + " at " + elevator.currentFloor() + ": servicing waitingQueue, going to floor " + waitingQueue[0] + ", because someone is waiting there.");
           var destinationFloorNo = waitingQueue.shift();
           elevator.goToFloor(destinationFloorNo);
-          setElevatorDirectorIndicator(elevator,destinationFloorNo)
+          //setElevatorDirectorIndicator(elevator,destinationFloorNo)
         } else {
-            console.log("no-one is waiting, going to 0");
+            console.log("Elevator " + elevator.id + " at " + elevator.currentFloor() + ": no-one is waiting, going to 0");
             elevator.goToFloor(0);
         }
 				// next step: if no-one is waiting, go to a 'strategic' floor to wait for passengers
+
+        printElevatorQueue(elevator);
 			});
 
 			elevator.on("floor_button_pressed",function(floorNum){
 				// add floor to queue
 				// set direction indicator via goingUpIndicator and goingDownIndicator
-				console.log("I want to go to floor: " + floorNum);
+				console.log("Elevator " + elevator.id + " at " + elevator.currentFloor() + ": someone got in and wants to go to floor: " + floorNum);
 
 				// add to queue, make sure it is in the order in which we are travelling, and continue travel
 				elevator.destinationQueue.push(floorNum);
@@ -41,53 +44,69 @@
 					elevator.destinationQueue.reverse();
 
 				elevator.checkDestinationQueue();
+
+        printElevatorQueue(elevator);
 			});
 
-			elevator.on("passing_floor",function(floorNum, direction){
-				// what to do when we are passing a floor?
-			});
+  			elevator.on("passing_floor",function(floorNum, direction){
+  				// what to do when we are passing a floor?
+  			});
 
-			elevator.on("stopped_at_floor",function(floorNum){
-				// people can get in here; if the waitingQueue says there is someone, remove the request from the queue.
-        if(waitingQueue[0] == floorNum)
+  			elevator.on("stopped_at_floor",function(floorNum){
+  				// people can get in here; if the waitingQueue says there is someone, remove the request from the queue.
+          console.log("Elevator " + elevator.id + " at " + elevator.currentFloor() + ": Stopping at floor " + floorNum);
+          if(waitingQueue[0] == floorNum) {
+            console.log("Elevator " + elevator.id + " at " + elevator.currentFloor() + ": We're at the same floor as the head of the waitingQueue, removing");
 				    waitingQueue.shift();
+          }
+
+          printElevatorQueue(elevator);
+          printWaitingQueue();
 			});
 		}
 
 		function registerFloorEvents(floor) {
-			floor.on("up_button_pressed ",function(){
+			floor.on("up_button_pressed",function(){
 				dispatchElevator(floor.floorNum(),"up");
+        console.log("up_button_pressed at floor: " + floor.floorNum());
 			});
-			floor.on("down_button_pressed ",function(){
+			floor.on("down_button_pressed",function(){
 				dispatchElevator(floor.floorNum(),"down");
+        console.log("down_button_pressed at floor: " + floor.floorNum());
         	});
 		}
 
 		function dispatchElevator(floorNum, direction) {
 			// first check if there is an elevator moving, and already passing the floor in the right direction
-			var passingElevators = findAvailableOrPassingElevator(floorNum,direction);
+			var passingElevators = findPassingElevator(floorNum,direction);
+      console.log("Dispatch - # passing elevators found: " + passingElevators.toString());
 			//passingElevators.sort(function(a,b){a.loadFactor()-b.loadFactor()});
 			if(passingElevators.length>0) {
         // so there is one that is passing by, reroute the elevator to also stop at this floor
-				console.log("one is already passing by!");
+				console.log("Request: " + floorNum + ", " + direction + ": One is already passing by! Elevator is rerouted.");
 				passingElevators[0].destinationQueue.unshift(floorNum);
+        console.log("Destqueue length: " + passingElevators[0].destinationQueue)
         passingElevators[0].checkDestinationQueue();
+
+        printElevatorQueue(passingElevators[0]);
 			} else {
 				// otherwise signal that someone is waiting at this floor; push to end of the queue
-				console.log("find idle elevator for floor: " + floorNum);
+				console.log("Request: " + floorNum + ", " + direction + ": queue-ing for floor: " + floorNum);
 				waitingQueue.push(floorNum);
+        console.log
 			}
+      printWaitingQueue();
 
 		}
 
-  function findAvailableOrPassingElevator(fromFloorNo, direction){
+  function findPassingElevator(fromFloorNo, direction){
     var availableElevators;
 
-    availableElevators = elevators.filter(function(elevator) {return elevator.destinationDirection === direction});
+    availableElevators = elevators.filter(function(elevator) {return elevator.destinationDirection() === direction});
     if(direction === "up")
-      availableElevators = availableElevators.filter(function(elevator) {return elevator.currentFloor < fromFloorNo});
+      availableElevators = availableElevators.filter(function(elevator) {return elevator.currentFloor() < fromFloorNo});
     else
-      availableElevators = availableElevators.filter(function(elevator) {return elevator.currentFloor > fromFloorNo});
+      availableElevators = availableElevators.filter(function(elevator) {return elevator.currentFloor() > fromFloorNo});
 
     return availableElevators;
   }
@@ -119,6 +138,14 @@
         setElevatorGoingDown(elevator);
       else
         setElevatorGoingUp(elevator);
+    }
+
+    function printElevatorQueue(elevator) {
+      console.log("Elevator " + elevator.id + " at " + elevator.currentFloor() + " queue: " + elevator.destinationQueue.toString());
+    }
+
+    function printWaitingQueue() {
+      console.log("Waiting Queue: " + waitingQueue.toString());
     }
 
 	},
